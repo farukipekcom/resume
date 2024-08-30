c9aa45893053f4a8379ae9f35ea2534c82870fef
farukipekcom <farukipekweb@gmail.com>
2024-08-29 22:09:30 -0500

    added about section

---
+++ app/components/available.js
@@ -0,0 +1,10 @@
+export default function Available() {
+  return (
+    <a href="mailto:faruk@farukipek.com" className="available">
+      <div className="dot">
+        <div className="signal"></div>
+      </div>
+      AVAILABLE FOR WORK
+    </a>
+  );
+}
---
+++ app/components/intro.js
@@ -0,0 +1,20 @@
+/* eslint-disable react/no-unescaped-entities */
+import Available from "./available";
+import Icons from "./icons";
+
+export default function Intro() {
+  return (
+    <div>
+      <div className="heading">
+        <div className="name">Faruk Ipek</div>
+        <Available />
+      </div>
+      <div className="role">Front-end Developer</div>
+      <div className="about">
+        I'm a front-end developer from Chicago, USA. I've been designing and developing websites since 2016. I have collaborated with
+        clients across different countries and industries, creating diverse web solutions.
+      </div>
+      <Icons />
+    </div>
+  );
+}
